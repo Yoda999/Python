4549dda96891a61befe67e5c4d52f942649783cc
Yoda999 <35702156+Yoda999@users.noreply.github.com>
2023-12-24 20:35:00 +0100

    client/server test

--- TCP/tcp_server.py
+++ TCP/tcp_server.py
@@ -1,28 +1,20 @@
 import socket
-import threading
-IP = '0.0.0.0'
-PORT = 9998 
 
-def main():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(ip,PORT)
-    server.listen(5)
-    print(f'[*] listening on port {ip} {port}')
+HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+PORT = 65433  # Port to listen on (non-privileged ports are > 1023)
 
-while True:
-    client,address = server.accept()
-    print(f'[*] server accepted connection form {address[0]}:{address[1]}')
-    client_handler = threading.Thread(target = handle_client, args = client)
-    client_handler.start()
-    
-def handle_client(client_socket):
-    with client_socket as sock:
-        request = sock.recv(1024)
-        print(f'[*] Received: {request.decode("utf-8")}')
-        sock.send(B'ACK')
-
-if __name__ == '__main__':
-    main()
-    
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    print("server started---") 
+    s.listen()
+    print("server listening---")
+    conn, addr = s.accept()
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
     
 
